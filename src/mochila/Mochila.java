65cacd5ae4327c07adafc9a5b12a777f35698aba
Alejandro Puentes Camargo <brianpc200@gmail.com>
2020-05-30 22:33:49 -0500

    Mochila arreglada

--- src/mochila/Mochila.java
+++ src/mochila/Mochila.java
@@ -3,50 +3,49 @@ package mochila;
 import java.util.Scanner;
 
 public class Mochila {
+    public int max(int n, int m){
+        if (n>m){
+            return n;
+        }else {
+        return m;
+        }
+    }
     public static void main(String[]args){
-        
+        Mochila moc = new Mochila();
         Scanner cs = new Scanner(System.in); //Número de objetos
         System.out.println("Numero de objetos");
         int n = cs.nextInt();
         System.out.println("Peso de la mochila");
         int w = cs.nextInt();
         
-        int[][] v = new int[n+1][w+3]; //Matriz que almacenará los datos
-        
+        int[][] v = new int[n+1][w+1]; //Matriz que almacenará los datos
+        int [] peso = new  int [n+1];
+        int [] valor = new  int [n+1]; 
         //Llena de 0 la matriz de productos
-        
+        peso[0]=0;
+        valor[0]=0;
+        System.out.println("dijite los pesos y sus respectivos valores");
+        for (int i = 1; i <=n; i++) {
+            peso[i]=cs.nextInt();
+            valor[i]=cs.nextInt();
+        }
         for(int i = 0; i<n+1; i++){
-            for(int j = 0; j<2; j++){
+            for(int j = 0; j<w+1; j++){
                 v[0][j] = 0;
-                v[i][2] = 0;
+                v[i][0] = 0;
             }
         }
         
-        //Pide el valor de los productos
-        
-        for(int i = 1; i<n+1; i++){
-            for(int j = 0; j<2; j++){
-                System.out.println("["+i+"]"+"["+j+"]");
-                v[i][j] = cs.nextInt();
-            }
-        }
+       
         
         //Calcula los valores
         
         for(int i = 1; i<n+1; i++){
-            for(int j = 3; j<w+3; j++){
-                int wi = v[i][0];
-                int bi = v[i][1];
-                if(wi<=j){
-                    if((bi + v[i-1][j-wi]) > v[i-1][j]){
-                        v[i][j] = bi +v[i-1][j-wi];
-                    }
-                    else{
-                        v[i][j] = v[i-1][j];
-                    }
-                }
-                else{
-                    v[i][j] = v[i-1][j];
+            for(int j = 1; j<w+1; j++){
+                if(j>=peso[i]){
+                    v[i][j]=moc.max(v[i-1][j],v[i-1][j-peso[i]]+valor[i]);
+                }else{
+                    v[i][j]=v[i-1][j];
                 }
             }
         }
@@ -54,7 +53,7 @@ public class Mochila {
         //Imprime la matriz de datos
         
         for(int i = 0; i<n+1; i++){
-            for(int j = 0; j<w+3; j++){
+            for(int j = 0; j<w+1; j++){
                 System.out.print(v[i][j]);
             }
             System.out.println("  ");
